8fd8173e9385c761c0c703ff760a4197cfa7192a
jason <jason_ma1982@qq.com>
2025-09-15 23:11:05 +0800

    添加前端页面react构建，添加异步文件上传，信息展示，服务器信息展示等等

--- src/main/java/com/aliyun/rag/model/DocumentRequest.java
+++ src/main/java/com/aliyun/rag/model/DocumentRequest.java
@@ -11,22 +11,22 @@ import jakarta.validation.constraints.NotNull;
  * 用于接收文档上传请求的参数模型
  * 支持文件上传和相关元数据的提交
  * </p>
- * 
+ *
  * @author Jason Ma
  * @version 1.0.0
  * @since 2025-09-09
  */
 @Data
 public class DocumentRequest {
-    
+
     @NotNull(message = "文件不能为空")
     private MultipartFile file;
-    
+
     private String title;
-    
+
     private String description;
-    
+
     private String tags;
-    
+
 
 }
--- src/main/java/com/aliyun/rag/model/SearchResult.java
+++ src/main/java/com/aliyun/rag/model/SearchResult.java
@@ -8,25 +8,25 @@ import lombok.Data;
  * 用于封装知识库搜索的结果数据
  * 包含匹配的文档内容、相似度分数和元数据信息
  * </p>
- * 
+ *
  * @author Jason Ma
  * @version 1.0.0
  * @since 2025-09-09
  */
 @Data
 public class SearchResult {
-    
+
     private String fileRecordId;
-    
+
     private String title;
-    
+
     private String content;
-    
+
     private Double score;
-    
+
     private String source;
-    
+
     private Integer position;
-    
+
 
 }
